c286eeb98fb18d8a730ae223aa8a381bbf42b7aa
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-03-27 05:41:18 +0000

    basics

---
+++ .devcontainer/basics/test.js
@@ -0,0 +1,8 @@
+console.log("alok");
+
+let accountemail = "alokkanojiya1@gmail.com"
+let accountpassword = "123456"
+let accoutcity ="mumbai"
+let accountstate = "Maharashtra"
+
+console.table( [accountemail, accountpassword, accoutcity, accountstate] )
