432d9a8ce6c06d77e8f727f05b15bba2f5431214
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-03-27 19:08:24 +0000

    bas

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,19 @@
+let score = 100
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isloggedin = 0
+
+let bolleanisloggedin = Boolean(isloggedin)
+console.log(bolleanisloggedin);
+
+let somenumber = 50
+
+let stringumber = String(somenumber)
+console.log(stringumber);
+console.log(typeof stringumber);
+
+
