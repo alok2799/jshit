b92e116b361460826fafce4665c72a3082e34e3f
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-04-02 19:09:25 +0000

    basics

---
+++ .devcontainer/basics/datatypes-sum.js
@@ -0,0 +1,21 @@
+// stack (primitive), Heap (non-premative)
+let myname = "alok"
+
+let anothername = myname
+anothername = "tony"
+
+console.log(anothername);
+console.log(myname);
+
+let user1 = {
+    email: "alokkanojiya1@gmial.com",
+    mobile: 8446616709
+
+}
+let user2 = user1
+ 
+    user2.email = "tony@gmail.com"
+
+console.log(user1);
+console.log(user2);
+
