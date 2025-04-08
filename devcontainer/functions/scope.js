90b0415d49a752371a996891424142ceae55a828
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-04-08 19:20:29 +0000

    scope

---
+++ devcontainer/functions/scope.js
@@ -0,0 +1,18 @@
+if (true) {
+    const username = "Alok";
+    if (username === "Alok") {
+        const website =" youtube"
+        // console.log(username + website);
+        
+    }
+}
+
+function addone(num) {
+    return num + 1
+}
+console.log(addone(5));
+
+const addtwo = function(num) {
+    return num + 2
+}
+console.log(addtwo(5));
