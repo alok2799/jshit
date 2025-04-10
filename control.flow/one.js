712003f23f892a3051068a7c77b8725db98c5fd2
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-04-10 05:53:40 +0000

    logged in

---
+++ control.flow/one.js
@@ -0,0 +1,50 @@
+// const Temprature = 50
+
+// if (Temprature >= 60) {
+//     // console.log("less than 50");
+    
+// }
+// else {
+//     // console.log("greater than 50");
+// }
+
+
+/////////////////////////////////////
+// const score = 200
+// if (score > 100) {
+//     let power = "Fly"
+//     console.log(`user power: ${power}`);
+// }
+
+/////////////////////////////////////
+// const balance = 1000
+// // if (balance > 500) console.log("tested");
+
+// if (balance < 500 ) {
+//     console.log("less than 500");
+// }
+// else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+
+/////////////////////////////////
+// const  UserLoggedIn = true
+// const debitcard = true
+// const loggedinfromgoogle = true
+// const loggedinfromfacebook = true
+
+// if (UserLoggedIn && debitcard) {
+//     console.log("login Succesfull, Allow to by a courss");
+// }
+// if (loggedinfromgoogle || loggedinfromfacebook) {
+//     console.log("login Aceepted");
+    
+// }
+
+
