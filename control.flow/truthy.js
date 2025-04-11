cbdb6e61a16c3c6f927b3f773efce871c8544d58
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-04-11 19:17:24 +0000

    nulis coalescing oprators

---
+++ control.flow/truthy.js
@@ -0,0 +1,23 @@
+//nulis coalescing oprators (??) null undefined
+
+let val1;
+val1 = 6 ?? 10 // the first value will print unless if we dont put null and undefined first
+
+let val2;
+val2 = null ?? "great share"  //this can help for linkedin automatic or manual comments
+
+let val3;
+val3 = undefined ?? "good post"  //this can help for linkedin automatic or manual comments
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+
+// Terniary opreators
+
+// condition ? True : false
+
+const iceteaprice = 100
+iceteaprice >= 80 ? console.log("more than 80"): console.log("less than 80");
+
+
