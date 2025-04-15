f8179b40e6c6d87aeb00796c5317276032c84c3a
alok2799 <163736690+alok2799@users.noreply.github.com>
2025-04-15 05:13:56 +0000

    loops

---
+++ loops/loops.js
@@ -0,0 +1,49 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     // console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         // console.log("5 is best num");
+        
+//     }
+//     // console.log(element);
+// }
+
+
+// for (let i = 1; i <= 5 ; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 1; j <= 5; j++) {
+//         // console.log(`inner loop value: ${j} and inner loop ${i}`);
+//         // console.log(i + "*" + j + "=" + i*j);  
+//     }  
+// }
+
+// let myarray = ["iron", "tonystark", "spiderman", "batman"]
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     // console.log(element);
+    
+    
+// }
+///////////////////////////////////////////////
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }  
+//     console.log(`value of i is ${index}`);  
+// }
+
+////////////////////////////////////////////////
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }  
+    console.log(`value of i is ${index}`);  
+}
+
+
